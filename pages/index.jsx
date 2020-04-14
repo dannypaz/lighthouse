30740ebc461f0d06a38d277c7f53d39fb633f3c1
dannypaz <danny@sparkswap.com>
2020-04-13 21:44:39 -0700

    first time

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <div>
+      <p>Hello Next.js</p>
+      <video id="lighthouse-local" autoPlay playsInline></video>
+      <script src="/main.js" />
+    </div>
+  )
+}
